ec478a2c78bb53c606f377a4bc003af4628eea56
alarconm <malarconii@yahoo.com>
2017-01-28 21:28:35 -0800

    Fully functional but not stylish!

--- WikiViewer.js
+++ WikiViewer.js
@@ -1,14 +1,19 @@
-$(document).ready(function () {});
-    var search = document.getElementById("userSearch").value;
-            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&list=search&srsearch=" + search + "&format=json&callback=?",function (x) {
-                
-        var link = document.createElement('x');
-                      
-                for (i = 0; i < 10; i++) {  
+$(document).ready(function () {
+    //start on click to avoid loading too soon
+    $("#submitBtn").click(function()
+    {
+        //empty the result so that it doesn't keep adding 10 results on top of each other
+        $("#results").empty();
+    var resultSearch = $("#userSearch").val();  
+        $("#dom_element").val(resultSearch);
+    //query the wikimedia API with a callback to get it back in JSON
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&list=search&srsearch=" + resultSearch + "&format=json&callback=?",function (x) {        
+        var link = document.createElement('x'); 
+                //append each result up to a maximum of 10 with a link to its wiki page
+                for (i = 0; i < 10; i++)    {  
                     var titleLink =  x.query.search[i].title.replace(/ /g, "_");
             $("#results").append("<div class= well>" + "<a href=https://en.wikipedia.org/wiki/" + titleLink + " target=_blank>" +  x.query.search[i].title + ": " + x.query.search[i].snippet + "</a>" + "</div>" + "<p />"); 
-                console.log(search);                      
-                 //   document.getElementbyId("results").appendChild(results);
-                 //   $("#results").html(JSON.stringify(x));
-                }                    
-}); 
+                                            }                    
+                                                    }); 
+          });
+                                });
