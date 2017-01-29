b49f0c034368b4903280ca66b9f860ce2b44ef44
alarconm <malarconii@yahoo.com>
2017-01-28 16:36:59 -0800

    Add files via upload

--- WikiViewer.js
+++ WikiViewer.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
- 
-    $.ajax({
-  url: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json',
-  data: { action: 'wbgetentities', ids: mw.config.get('wgWikibaseItemId'), format: 'json' },
-  dataType: 'jsonp',
-  success: function (x) {
-    var link = document.createElement('x');
-      $("#results").html(x.);
-  }
-});
-
-});
+$(document).ready(function () {});
+    var search = document.getElementById("userSearch").value;
+            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&list=search&srsearch=" + search + "&format=json&callback=?",function (x) {
+                
+        var link = document.createElement('x');
+                      
+                for (i = 0; i < 10; i++) {  
+                    var titleLink =  x.query.search[i].title.replace(/ /g, "_");
+            $("#results").append("<div class= well>" + "<a href=https://en.wikipedia.org/wiki/" + titleLink + " target=_blank>" +  x.query.search[i].title + ": " + x.query.search[i].snippet + "</a>" + "</div>" + "<p />"); 
+                console.log(search);                      
+                 //   document.getElementbyId("results").appendChild(results);
+                 //   $("#results").html(JSON.stringify(x));
+                }                    
+}); 
