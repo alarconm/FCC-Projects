d2a315f0a8420d9f031f0ffd2baad6c5c1c04b13
Michael Alarcon <malarconii@yahoo.com>
2017-06-04 15:08:41 -0700

    added countdown timer, not working yet

--- Pomodoro.js
+++ Pomodoro.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
     //class contains functions & variables for each part of clock
     class CountTimer {
         constructor() {
-            this.count = 5;
+            this.count = 25;
         }
 
         plus() {
@@ -19,6 +19,26 @@ $(document).ready(function (){
     var breakLength = new CountTimer();
     var sessionLength = new CountTimer();
     var pomodoro = new CountTimer();
+    breakLength.count = 5;
+
+    $("#pomodoro").on("click", function(){
+        function update() {
+        var myTime = sessionLength.count;
+        var ss = myTime.split(":");
+        var dt = new Date();
+        dt.setHours(0);
+        dt.setMinutes(ss[0]);
+        dt.setSeconds(ss[1]);
+
+        var dt2 = new Date(dt.valueOf() + 1000);
+        var temp = dt2.toTimeString().split(" ");
+        var ts = temp[0].split(":");
+
+        $("#pomodoro").html(ts[1]+":"+ts[2]);
+        setTimeout(update, 1000);
+
+        }
+    });
 
     $("#breakminus").on("click", function(){
         breakLength.minus();
@@ -32,10 +52,13 @@ $(document).ready(function (){
     $("#sessionminus").on("click", function(){
         sessionLength.minus();
         $("#sessionTimer").html(sessionLength.count);
+            $("#pomodoro").html(sessionLength.count);
 
     });
     $("#sessionplus").on("click", function(){
         sessionLength.plus();
         $("#sessionTimer").html(sessionLength.count);
+            $("#pomodoro").html(sessionLength.count);
     });
+
 });
